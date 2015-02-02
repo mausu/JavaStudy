b1f7cbd2771732d44d3670a4b311caffe8d5e0ed
mausu <mausukun_1217@hotmail.co.jp>
2015-02-03 00:35:13 +0900

    初コミット

---
+++ src/net/mausukun/java/Chapter5/exercise1/ArraysExample.java
@@ -0,0 +1,29 @@
+package net.mausukun.java.Chapter5.exercise1;
+
+import java.util.Arrays;
+import java.util.Random;
+
+class ArraysExample {
+	public static void main(String[] args) {
+		double[] nums = new double[10];
+		Random random = new Random();
+		for(int i=0;i<nums.length;i++){
+			nums[i] = random.nextFloat();
+		}
+		myPrintln(nums);
+		System.out.println("\n");
+		Arrays.sort(nums);
+		myPrintln(nums);
+		System.out.println("\n");
+		int result = Arrays.binarySearch(nums, 0.13);
+		System.out.println(result + "\n");
+		Arrays.fill(nums, 1);
+		myPrintln(nums);
+	}
+	
+	static void myPrintln(double[] array){
+		for(int i=0;i<array.length;i++){
+			System.out.println("[" + i + "] = " + array[i]);
+		}
+	}
+}
